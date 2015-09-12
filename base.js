bae10ce892130d48f86c91a25f5481f9e849abc6
Austin Ezell <austin.r.ezell@gmail.com>
2015-09-12 11:55:00 -0700

    typo correction

--- base.js
+++ base.js
@@ -2,7 +2,7 @@ go()
 
 function go() {
   var gameRef = new Firebase("https://battleship-2015.firebaseio.com/");
-  var usa = confirm('USA good or click cancel for Mother Russia');
+  var usa = confirm('USA good? or "nah" for Mother Russia');
   var userId = usa ? "usa" : "ussr"
   console.log(userId);
 };
