0af607a2a3499a75ebe3bff8b015affdbd3d9014
austin ezell <austin.r.ezell@gmail.com>
2015-12-08 18:49:27 -0800

    removal of unused js file

--- base.js
+++
@@ -1,9 +0,0 @@
-// var Firebase = require('main.js');
-go()
-
-function go() {
-  var gameRef = new Firebase("https://battleship-2015.firebaseio.com/");
-  var usa = confirm('USA good? or "nah" for Mother Russia');
-  var userId = usa ? "usa" : "ussr"
-  console.log(userId);
-};
